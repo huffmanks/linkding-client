09edf0b3c789064974122de58581c7fecb2b4e0f
Kevin Huffman <huffmanks2@gmail.com>
2026-03-25 17:54:17 -0400

    init settings form tabs

--- src/routes/(protected)/dashboard/settings/index.tsx
+++ src/routes/(protected)/dashboard/settings/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { SettingsForm } from "@/components/forms/settings-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const Route = createFileRoute("/(protected)/dashboard/settings/")({
   component: RouteComponent,
@@ -10,7 +11,29 @@ function RouteComponent() {
   return (
     <div className="max-w-lg px-4 pt-4 pb-8 md:pt-0">
       <h1 className="mb-4 text-2xl font-medium">Settings</h1>
-      <SettingsForm />
+
+      <Tabs defaultValue="user">
+        <TabsList>
+          <TabsTrigger value="user">User</TabsTrigger>
+          <TabsTrigger value="ui">UI</TabsTrigger>
+          <TabsTrigger value="bookmark">Bookmark</TabsTrigger>
+          <TabsTrigger value="cache">Cache</TabsTrigger>
+        </TabsList>
+        <div className="pt-4">
+          <TabsContent value="user">
+            <SettingsForm />
+          </TabsContent>
+          <TabsContent value="ui">
+            <SettingsForm />
+          </TabsContent>
+          <TabsContent value="bookmark">
+            <SettingsForm />
+          </TabsContent>
+          <TabsContent value="cache">
+            <SettingsForm />
+          </TabsContent>
+        </div>
+      </Tabs>
     </div>
   );
 }
